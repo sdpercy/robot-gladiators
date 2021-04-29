5638241c6519b3faf42e470455618002bc940658
sdpercy <scottpercy@hotmail.com>
2021-04-29 14:15:26 -0230

    interger response in shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -154,25 +154,22 @@ var endGame = function() {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
 
   // use switch case to carry out action
+  // use switch case to carry out action
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
-  case "REFILL":
-  case "refill":
-    playerInfo.refillHealth();
-    break;
-case "UPGRADE":
-case "upgrade":
-  playerInfo.upgradeAttack();
-  break;
-case 'LEAVE':
-case 'leave':
-    window.alert('Leaving the store.');
-
-    // do nothing, so function will end
-    break;
+    case 1:
+      playerInfo.refillHealth();
+      break;
+    case 2:
+      playerInfo.upgradeAttack();
+      break;
+    case 3:
+      window.alert("Leaving the store.");
+      break;
     default:
       window.alert('You did not pick a valid option. Try again.');
 
